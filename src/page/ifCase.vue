77d5fb01f4b88d29fcdda0cbf277e47e9c62674b
biexei@163.com <xiebei123>
2020-10-09 21:44:56 +0800

    fix

--- src/page/ifCase.vue
+++ src/page/ifCase.vue
@@ -585,7 +585,7 @@
                     <el-col :span="5">
                         <el-input v-model="assertItem.expression" placeholder="提取表达式"  size="small"></el-input>
                     </el-col> 
-                    <el-col :span="2">
+                    <el-col :span="3">
                         <el-select v-model="assertItem.operator" size='small'>
                           <el-option
                             v-for="item in assertOperatorOptions"
@@ -595,7 +595,7 @@
                           </el-option>
                         </el-select>
                     </el-col> 
-                    <el-col :span="6">
+                    <el-col :span="5">
                         <el-input v-model="assertItem.exceptedResult" placeholder="预期结果"  size="small"></el-input>
                     </el-col> 
                     <el-col :span="2">
